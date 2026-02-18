f6de83f195d2cbfde682cb2c010a003ac321e2c9
GuiMM <guilherme.macedomoreira@gmail.com>
2026-02-18 19:53:55 -0300

    Aplica framework langchain para padronizar e desacoplar uso de ferramenta LLM

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from openai import OpenAI
+
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import PromptTemplate
 from dotenv import load_dotenv
 import os
 
@@ -7,32 +9,27 @@ load_dotenv()
 api_key = os.getenv("OPENAI_API_KEY")
 
 numero_dias = 7
-
 numero_criancas = 2
+atividades = "praia"
 
-atividades = "praia, parque, museus"
-
-prompt = f"Crie um roteiro de viagem de {numero_dias} dias, para uma família com {numero_criancas} crianças, que gosta de {atividades} "
-
-
-
-cliente = OpenAI(api_key=api_key)
-
-resposta = cliente.chat.completions.create(
-    model="gpt-3.5-turbo",
-    messages=[
-        #caracterizacao
-        {
-            "role": "system", 
-            "content": "Você é um assistente de roteiro de viagens."
-        },
-        #Quem está interagindo
-        {
-            "role": "user", 
-            "content": prompt
-        }
-    ]
+modelo_de_prompt = PromptTemplate(
+    template="""
+    Crie um roteiro de viagem de {dias} dias,
+    para uma família com {criancas} criancas,
+    que gostam de {atividades}.
+    """
 )
 
-resposta_em_texto = resposta.choices[0].message.content
-print(resposta)
+prompt = modelo_de_prompt.format(
+    dias=numero_dias, 
+    criancas=numero_criancas, 
+    atividades=atividades
+)
+
+modelo = ChatOpenAI(model="gpt-3.5-turbo",
+                    temperature=0.5,  
+                    api_key=api_key)
+
+resposta = modelo.invoke(prompt)
+
+print(resposta.content)
