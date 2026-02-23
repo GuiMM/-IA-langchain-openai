9f4361a10c9a8889b0f0fb14fb9a181bc16b7c0a
GuiMM <guilherme.macedomoreira@gmail.com>
2026-02-23 19:42:25 -0300

    Orquestrando assistentes com lang graph

--- main_langgraph.py
+++ main_langgraph.py
@@ -1,9 +1,12 @@
 import os
+import asyncio
 from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.globals import set_debug
+from langgraph.graph import StateGraph, START, END
+from langchain_core.runnables import RunnableConfig
 from typing import Literal, TypedDict
 
 set_debug(False)
@@ -44,15 +47,42 @@ prompt_roteador = ChatPromptTemplate.from_messages(
 
 roteador = prompt_roteador | modelo.with_structured_output(Rota)
 
-def responda(pergunta:str):
-    rota=roteador.invoke({"query": pergunta})["destino"]
-    print(rota)
-    if rota == "cultural":
-        return cadeia_cultura.invoke({"query": pergunta})
-    elif rota == "montanha":
-        return cadeia_montanha.invoke
+class Estado(TypedDict):
+    query: str
+    destino: Rota
+    resposta: str
 
-print(responda(
-     "Quero visitar um lugar no Brasil, famoso por praias e cultura. Pode sugerir?"
+async def no_roteador(estado: Estado, config: RunnableConfig):
+    return {"destino": await roteador.ainvoke({"query":estado["query"]}, config=config) }
+
+async def no_cultural(estado: Estado, config: RunnableConfig):
+    return {"resposta": await cadeia_cultura.ainvoke({"query":estado["query"]}, config=config) }
+
+async def no_montanha(estado: Estado, config: RunnableConfig):
+    return {"resposta": await cadeia_montanha.ainvoke({"query":estado["query"]}, config=config) }
+
+
+def escolher_no(estado:Estado) -> Literal["cultural","montanha"]:
+    return "cultural" if estado["destino"]["destino"] == "cultural" else "montanha"
+
+grafo = StateGraph(Estado)
+grafo.add_node("rotear", no_roteador)
+grafo.add_node("cultural", no_cultural)
+grafo.add_node("montanha", no_montanha)
+
+grafo.add_edge(START, "rotear")
+grafo.add_conditional_edges("rotear", escolher_no)
+grafo.add_edge("cultural", END)
+grafo.add_edge("montanha", END)
+
+app = grafo.compile()
+
+async def main():
+    resposta = await app.ainvoke(
+        {"query": "Quero visitar um lugar no Brasil, famoso por praias e cultura. Pode sugerir?"}
     )
+    print(resposta)
+
+asyncio.run(
+    main()
 )
